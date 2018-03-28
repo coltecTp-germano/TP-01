f3621d865a588499a0c93fa7ece1cd2a0c23bc72
ibryans <bryans0701@gmail.com>
2018-03-28 11:25:02 -0300

    Desenvolvendo o layout do cadastro

--- app/src/main/java/coltectp/github/io/movieme/CadastroActivity.java
+++ app/src/main/java/coltectp/github/io/movieme/CadastroActivity.java
@@ -1,14 +1,13 @@
 package coltectp.github.io.movieme;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro);
     }
 }
