dffd6b1836dfa96c36437bf238a21c056f91e547
Germano Barcelos <germanobarcelos@yahoo.com.br>
2018-03-28 10:19:19 -0300

    Adicionando floating Button

---
+++ app/src/main/java/coltectp/github/io/movieme/MainActivity.java
@@ -0,0 +1,13 @@
+package coltectp.github.io.movieme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
